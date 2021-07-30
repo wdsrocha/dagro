6ca92cb5090eac357ff85a84de743c68e2632ca8
Wesley Rocha <wesleysr1997@gmail.com>
2021-07-30 02:08:46 -0400

    Allow data update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import { ethers } from "ethers";
-import { Greeter__factory } from "../contracts/types";
+import { Greeter, Greeter__factory } from "../contracts/types";
 
 const GREETER_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
@@ -8,6 +8,18 @@ const Index: FC = () => {
   const [provider, setProvider] = useState<ethers.providers.Web3Provider>();
   const [account, setAccount] = useState<string>();
   const [greeting, setGreeting] = useState<string>();
+  const [greetingText, setGreetingText] = useState<string>("");
+  const [greeter, setGreeter] = useState<Greeter>();
+
+  useEffect(() => {
+    if (provider && account) {
+      const greeter = Greeter__factory.connect(
+        GREETER_ADDRESS,
+        provider.getSigner()
+      );
+      setGreeter(greeter);
+    }
+  }, [provider, account]);
 
   const connect = async () => {
     if (!window.ethereum?.request) {
@@ -25,14 +37,16 @@ const Index: FC = () => {
   };
 
   const greet = async () => {
-    if (provider && account) {
-      const greeter = Greeter__factory.connect(
-        GREETER_ADDRESS,
-        provider.getSigner()
-      );
-
+    if (greeter) {
       const greeting = await greeter.greet();
       setGreeting(greeting);
+    }
+  };
+
+  const updateGreeting = async () => {
+    if (greeter) {
+      const transaction = await greeter.setGreeting(greetingText);
+      await transaction.wait();
     }
   };
 
@@ -42,6 +56,12 @@ const Index: FC = () => {
       <p>Account: {account}</p>
       <button onClick={greet}>See greeting!</button>
       <p>{greeting}</p>
+      <input
+        value={greetingText}
+        onChange={(e) => setGreetingText(e.target.value)}
+      />
+      <button onClick={updateGreeting}>Update greeting</button>
+      <p>{greetingText}</p>
     </>
   );
 };
