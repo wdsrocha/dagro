9904ed75222f4e0e394a7b468558d321df210882
Wesley Rocha <wesleysr1997@gmail.com>
2021-08-02 16:12:12 -0400

    Add product to marketplace

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.6;
 contract Marketplace {
     constructor() {}
 
+    // ============================== ACCOUNT ============================== //
     struct Account {
         address id;
         string name;
@@ -13,6 +14,7 @@ contract Marketplace {
     }
 
     mapping(address => Account) public accounts;
+    address[] public accountList;
 
     function updateAccount(
         string memory name,
@@ -25,5 +27,40 @@ contract Marketplace {
         accounts[msg.sender].email = email;
         accounts[msg.sender].phone = phone;
         accounts[msg.sender].postalCode = postalCode;
+        accountList.push(msg.sender);
+    }
+
+    // ============================== PRODUCT ============================== //
+    struct Product {
+        uint256 id;
+        string name;
+        string description;
+        uint256 price;
+        address payable sellerId;
+    }
+
+    mapping(uint256 => Product) public products;
+    uint256[] public productList;
+
+    function getProductCount() public view returns (uint256 productCount) {
+        return productList.length;
+    }
+
+    function addProduct(
+        string memory name,
+        string memory description,
+        uint256 price
+    ) public {
+        require(
+            accounts[msg.sender].id == msg.sender,
+            "Atualize seus dados para adicionar um produto"
+        );
+
+        uint256 i = productList.length;
+        products[i].id = i;
+        products[i].name = name;
+        products[i].description = description;
+        products[i].price = price;
+        productList.push(i);
     }
 }
--- test/Marketplace.test.ts
+++ test/Marketplace.test.ts
@@ -3,6 +3,7 @@ import chai from "chai";
 import chaiAsPromised from "chai-as-promised";
 import { Marketplace, Marketplace__factory } from "../types/ethers-contracts";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { formatEther, formatUnits, parseEther } from "ethers/lib/utils";
 
 chai.use(chaiAsPromised);
 const { expect } = chai;
@@ -28,15 +29,37 @@ describe("Marketplace", () => {
   });
 
   it.only("should work", async () => {
-    const [, buyer] = await ethers.getSigners();
-    const buyerContract = await contract.connect(buyer);
-    await buyerContract.updateAccount(
+    const signers = await ethers.getSigners();
+    const contracts: Marketplace[] = [];
+    for (const signer of signers) {
+      contracts.push(await contract.connect(signer));
+    }
+
+    await contracts[1].updateAccount(
       "Wesley Rocha",
       "wsr.snf17@uea.edu.br",
       "+55 (92) 9 8210-0440",
       "69050-120"
     );
 
-    console.log(await contract.accounts(buyer.address));
+    contracts[1].addProduct("Abacaxi (un.)", "Delicioso.", parseEther("5"));
+    contracts[1].addProduct("Alface (kg.)", "Fresquinho..", parseEther("3"));
+
+    const productCount = parseInt((await contract.getProductCount())._hex);
+    console.log({ productCount });
+    for (let i = 0; i < productCount; i++) {
+      console.log(await contract.products(i));
+    }
+
+    await contracts[2].updateAccount(
+      "Adham Lucas",
+      "adham.lucas@gmail.com",
+      "+55 (92) 9 8204-1337",
+      "69000-123"
+    );
+
+    // contracts.
+
+    // console.log(await contract.accounts(buyer.address));
   });
 });
